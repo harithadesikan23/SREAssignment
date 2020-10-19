a20bf5d7d80fb353ee41664f9f3c13b0fa6707a2
harithadesikan23 <haritha.desikan@outlook.com>
2020-10-19 19:48:16 -0400

    Update TestSRETool.py

--- TestSRETool.py
+++ TestSRETool.py
@@ -7,9 +7,9 @@ from SRETool import *
 class TestSRETool(unittest.TestCase):
     df = pd.read_csv('TestInput.csv')
     
-    def test_compute_similarity_score(self):
+    def test_compare_images(self):
         obj = SRETool()
-        output_df = obj.compute_similarity_score(TestSRETool.df)
+        output_df = obj.compare_images(TestSRETool.df)
         self.assertEqual(len(TestSRETool.df), len(output_df)) 
         columns = ["image1", "image2", "Similar", "Elapsed"]
         self.assertListEqual(list(output_df.columns), columns)
